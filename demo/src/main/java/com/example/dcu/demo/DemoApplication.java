df37d1c8525e3d6a0e269cbf6368cc55d5848657
daccukc <daccu@yahoo.com>
2020-03-29 16:07:43 -0500

    add HelloRestController and Greeting

--- demo/src/main/java/com/example/dcu/demo/DemoApplication.java
+++ demo/src/main/java/com/example/dcu/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.dcu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+@ComponentScan({"com.example.dcu.demo.controller"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ demo/src/main/java/com/example/dcu/demo/controller/HelloRestController.java
@@ -0,0 +1,40 @@
+package com.example.dcu.demo.controller;
+
+
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dcu.demo.domain.Greeting;
+
+
+@RestController  // this is @RestController
+//@RequestMapping("/api") // map /api
+//@CrossOrigin(origins = "http://localhost:3000") // need this to resolve CORS
+public class HelloRestController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+  
+}
